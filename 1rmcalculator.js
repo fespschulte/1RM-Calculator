ec63ae62f955854f063f8c7867252cd61f94e75d
Fernando Pinto <fschultepinto@gmail.com>
2022-08-24 21:23:31 -0300

    commit js

---
+++ 1rmcalculator.js
@@ -0,0 +1,8 @@
+function calcularRm(){
+  var weight = 0
+  var reps = 0
+  weight = parseFloat(document.getElementById('carga').value);
+  reps = parseFloat(document.getElementById('reps').value);
+  var umaRepMax = Math.round((100 * weight) / (101.3 - (2.67123 * reps)));
+  document.getElementById('answer').innerHTML='Resultado: ' + umaRepMax;
+}
