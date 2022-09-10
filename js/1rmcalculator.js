f94a12f38d7708e1df3e39796de856020875f172
Fernando Pinto <fschultepinto@gmail.com>
2022-09-10 15:39:51 -0300

    Ajuste da fórmula utilizada

--- js/1rmcalculator.js
+++ js/1rmcalculator.js
@@ -3,6 +3,6 @@ function calcularRm(){
   var reps = 0
   weight = parseFloat(document.getElementById('carga').value);
   reps = parseFloat(document.getElementById('reps').value);
-  var umaRepMax = Math.round((100 * weight) / (101.3 - (2.67123 * reps)));
+  var umaRepMax = Math.round(weight/((1.0278-(0.0278*reps))));
   document.getElementById('answer').innerHTML= umaRepMax + '&nbsp Kg';
 }
